4e2dde441f539eddbd5d57703d8b9d7d627f6a1f
MatiasJJ <39948154+MatiasJJ@users.noreply.github.com>
2018-08-09 14:08:21 +0300

    mac/linux

--- Manage_files/jobFromTxt.sh
+++ Manage_files/jobFromTxt.sh
@@ -10,8 +10,16 @@ echo "Jobfile: $7"
 
 cp $1 $7
 
+#mac
 sed -i '' "s/@core/$core/g" $7
 sed -i '' "s/@node/$node/g" $7
 sed -i '' "s/@time/$time/g" $7
 sed -i '' "s/@memcpu/$memcpu/g" $7
 sed -i '' "s/@jobname/$jobname/g" $7
+
+#linux
+sed -i "s/@core/$core/g" $7
+sed -i "s/@node/$node/g" $7
+sed -i "s/@time/$time/g" $7
+sed -i "s/@memcpu/$memcpu/g" $7
+sed -i "s/@jobname/$jobname/g" $7
